badaf68c6fae15caac070fa34351e687cec3feec
Tobintax <giulio09@marcov.it>
2020-03-25 23:18:40 +0100

    Login

--- src/App.js
+++ src/App.js
@@ -7,17 +7,25 @@ import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import 'firebase/analytics';
 
 import AppBar from '@material-ui/core/AppBar';
+import Avatar from '@material-ui/core/Avatar';
+import clsx from 'clsx';
+import Divider from '@material-ui/core/Divider';
 import Fab from '@material-ui/core/Fab';
 import Grid from '@material-ui/core/Grid';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
 
 import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import AddIcon from '@material-ui/icons/Add';
 import SearchIcon from '@material-ui/icons/Search';
-import MoreIcon from '@material-ui/icons/MoreVert';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+
+
 
 var firebaseConfig = {
   apiKey: "AIzaSyADxgU6pKy-sqxGhPHkqAoW_VqG85VsQB8",
@@ -43,6 +51,7 @@ const useStyles = makeStyles(theme => ({
   },
   list: {
     marginBottom: theme.spacing(2),
+    width: 250,
   },
   subheader: {
     backgroundColor: theme.palette.background.paper,
@@ -62,29 +71,89 @@ const useStyles = makeStyles(theme => ({
     right: 0,
     margin: '0 auto',
   },
+  fullList: {
+    width: 'auto',
+  },
+  root: {
+    display: 'flex',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
 }));
 
 function BottomAppBar() {
-  const classes = useStyles();
+  const [state, setState] = React.useState({
+    left: false,
+  });
+  const toggleDrawer = (anchor, open) => event => {
+    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
   return (
     <AppBar position="fixed" color="primary" className={useStyles().appBar}>
+      <SwipeableTemporaryDrawer state={state} setState={setState} toggleDrawer={toggleDrawer}/>
         <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="open drawer">
+          <IconButton edge="start" color="inherit" aria-label="open drawer" onClick={toggleDrawer("left", true)}>
             <MenuIcon />
           </IconButton>
           <Fab color="secondary" aria-label="add" className={useStyles().fabButton}>
             <AddIcon />
           </Fab>
           <div className={useStyles().grow} />
-          <IconButton color="inherit">
-            <SearchIcon />
-          </IconButton>
-          <IconButton edge="end" color="inherit">
-            <MoreIcon />
+          <Avatar alt={firebase.auth().currentUser.displayName} src={firebase.auth().currentUser.photoURL} />
+          <IconButton color="inherit" aria-label="logout" onClick={() => {firebase.auth().signOut()}}>
+            <ExitToAppIcon />
           </IconButton>
         </Toolbar>
       </AppBar>
   )
+}
+
+function SwipeableTemporaryDrawer(props) {
+  const classes = useStyles();
+  let state = props.state;
+  let toggleDrawer = props.toggleDrawer;
+
+  const list = anchor => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        <ListItem>
+          <div className={classes.root}>
+            <Avatar alt={firebase.auth().currentUser.displayName} src={firebase.auth().currentUser.photoURL} />
+          </div>
+        </ListItem>
+      </List>
+      <Divider />
+    </div>
+  );
+
+  return (
+    <div>
+      {['left'].map(anchor => (
+        <React.Fragment key={anchor}>
+          <SwipeableDrawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+            onOpen={toggleDrawer(anchor, true)}
+          >
+            {list(anchor)}
+          </SwipeableDrawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
 }
 
 
@@ -134,12 +203,12 @@ class SignInScreen extends React.Component {
             style={{ minHeight: '100vh' }}
           >
 
-            <Grid item xs={3}>
+            <Grid item xs={12}>
               <Typography variant="h1" component="h2">
               la spesa
               </Typography>
             </Grid>  
-            <Grid item xs={3}>
+            <Grid item xs={12}>
             <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()}/>
             </Grid>   
           </Grid>
@@ -150,9 +219,8 @@ class SignInScreen extends React.Component {
     return (
       <div>
         <BottomAppBar />
-        <h1>My App</h1>
-        <p>Welcome {firebase.auth().currentUser.displayName}! You are now signed-in!</p>
-        <a onClick={() => firebase.auth().signOut()}>Sign-out</a>
+        <h1>Home</h1>
+        <p>Benvenuto {firebase.auth().currentUser.displayName}</p>
       </div>
     );
   }
