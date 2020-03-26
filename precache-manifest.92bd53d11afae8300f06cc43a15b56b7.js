eeaa298e446628306446ca587d4c0cefcf4ddf25
Tobintax <giulio09@marcov.it>
2020-03-26 13:45:46 +0100

    Updates

--- precache-manifest.92bd53d11afae8300f06cc43a15b56b7.js
+++ precache-manifest.92bd53d11afae8300f06cc43a15b56b7.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "856c658065d79f8a6ff5c60c6a2338cd",
+    "revision": "16b24484d31afeddc8148f0139ed224b",
     "url": "/spesa/index.html"
   },
   {
-    "revision": "d9d1b80fe7d5dc9f136a",
+    "revision": "a929893065c1b2156899",
     "url": "/spesa/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "7036c1823185783d71c9",
-    "url": "/spesa/static/js/2.52340e7b.chunk.js"
+    "revision": "4b2eefcaf3bd3d4177bf",
+    "url": "/spesa/static/js/2.558df001.chunk.js"
   },
   {
     "revision": "1004cbdee6dd20bf2efff5879efc82a9",
-    "url": "/spesa/static/js/2.52340e7b.chunk.js.LICENSE.txt"
+    "url": "/spesa/static/js/2.558df001.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d9d1b80fe7d5dc9f136a",
-    "url": "/spesa/static/js/main.8b7f72a4.chunk.js"
+    "revision": "a929893065c1b2156899",
+    "url": "/spesa/static/js/main.8ea693e7.chunk.js"
   },
   {
     "revision": "6c501c2b1ab8205dc83b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spesa/precache-manifest.7e83c10e5b5faccd36a93bca330e35b8.js"
+  "/spesa/precache-manifest.92bd53d11afae8300f06cc43a15b56b7.js"
 );
 
 self.addEventListener('message', (event) => {
