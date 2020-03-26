d86b0a47c93ce5abb6b3371775dd7f54e4635e90
Tobintax <giulio09@marcov.it>
2020-03-26 12:36:18 +0100

    Updates

---
+++ precache-manifest.b2cc08e2851e1bc120df319db5b01931.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ceb37e819eeffdd4c525966ce355fa4",
+    "url": "/spesa/index.html"
+  },
+  {
+    "revision": "9426f14f270913d7e436",
+    "url": "/spesa/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7036c1823185783d71c9",
+    "url": "/spesa/static/js/2.52340e7b.chunk.js"
+  },
+  {
+    "revision": "1004cbdee6dd20bf2efff5879efc82a9",
+    "url": "/spesa/static/js/2.52340e7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9426f14f270913d7e436",
+    "url": "/spesa/static/js/main.ba7df9ad.chunk.js"
+  },
+  {
+    "revision": "6c501c2b1ab8205dc83b",
+    "url": "/spesa/static/js/runtime-main.fa788eee.js"
+  }
+]);
